0c45fcea72a009840e8b7b83fca45e0651502033
Daniel <speshik222@gmail.com>
2021-02-13 09:43:14 +0800

    Add time

--- friendly.py
+++ friendly.py
@@ -1,3 +1,6 @@
+import time
+start_time = time.time()
+
 def div(numb):
     sum1 = 0
     for i in range(1, numb):
@@ -12,3 +15,5 @@ for i in range(1, 10001):
     n = div(m)
     if (i == n) and (i < m):
         print(i, '', m)
+
+print("--- %s seconds ---" % (time.time() - start_time))
