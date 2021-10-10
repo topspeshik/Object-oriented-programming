9b3032d231356eedc84cd65f14ef5a1419c11e53
Daniel <speshik222@gmail.com>
2021-10-10 15:07:57 +0800

    oop++

---
+++ oop/oop.cpp
@@ -0,0 +1,87 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+class Number
+{
+	int value;
+
+
+public:
+	
+
+	Number()
+	{
+		value = 0;
+	}
+
+	Number(int valueF)
+	{
+		value = valueF;
+	}
+
+	
+
+	friend Number operator +(const Number& v1, const Number& v2);
+	
+	friend Number operator -(const Number& v1, const Number& v2);
+
+	friend Number operator /(const Number& v1, const Number& v2);
+
+	friend Number operator %(const Number& v1, const Number& v2);
+
+	friend Number operator *(const Number& v1, const Number& v2);
+
+	friend Number operator ^(const Number& v1, const Number& v2);
+
+	int getValue() { return value; }
+	
+};
+
+
+
+
+Number operator+(const Number& v1, const Number& v2)
+{
+	return Number(v1.value + v2.value);
+}
+
+Number operator-(const Number& v1, const Number& v2)
+{
+	return Number(v1.value - v2.value);
+}
+
+Number operator/(const Number& v1, const Number& v2)
+{
+	return Number(v1.value / v2.value);
+}
+
+Number operator%(const Number& v1, const Number& v2)
+{
+	return Number(v1.value % v2.value);
+}
+
+Number operator*(const Number& v1, const Number& v2)
+{
+	return Number(v1.value * v2.value);
+}
+
+Number operator^(const Number& v1, const Number& v2)
+{
+	return Number(pow(v1.value,v2.value));
+}
+
+
+
+
+int main()
+{
+	Number a(2);
+	Number b(3);
+
+	Number c =  b+a;
+	cout <<  c.getValue();
+
+}
